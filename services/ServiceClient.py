d4ff3357a37cc919c0d9b1ec2745ba7685b790ca
Wasin Alshami <100309594+AlshamiWasin@users.noreply.github.com>
2023-11-13 14:51:27 +0100

    Adding full client controller and service

--- services/ServiceClient.py
+++ services/ServiceClient.py
@@ -6,8 +6,6 @@ connection = db.db
 class ServiceClient:
     def getAllClients():
         return Client.query.all()
-    
-
 
     def getClientById(id_client):
         client = Client.query.get(id_client)
@@ -24,3 +22,21 @@ class ServiceClient:
         connection.session.commit()
         return new_client
     
+    def updateClient(client_id, new_nom, new_prenom , new_email, new_tel): 
+        client = Client.query.get(client_id)
+        if client:
+            client.nom_client = new_nom
+            client.prenom_client = new_prenom
+            client.email_client = new_email
+            client.telephone_client = new_tel
+            connection.session.commit()
+            return client
+        return None
+    
+    def deleteClient(client_id):
+        client = Client.query.get(client_id)
+        if client:
+            connection.session.delete(client)
+            connection.session.commit()
+            return True
+        return False
